b0aa5a4341f5a9e42c02f30e547f882eae53fa57
BosiDC <dexing30100@gmail.com>
2026-06-07 19:36:12 +1000

    init

---
+++ learning/variables/main.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+	var x string = "Hello Go!"
+	fmt.Println(x)
+
+	var y int = 42
+	fmt.Println(y)
+
+	var z bool = true
+	fmt.Println(z)
+
+	const pi float64 = 3.14
+	fmt.Println(pi)
+}
